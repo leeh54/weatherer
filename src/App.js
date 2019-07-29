6ef6920d10b8e0612c9327ea9bb3ba3fe369a8cd
Hyuk Gi Lee <hyukgilee@Hyuks-MacBook-Pro.local>
2019-07-28 22:12:59 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,63 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { makeStyles, withTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
+import { Bar } from 'react-chartjs-2'
+import moment from 'moment'
+import color from '@material-ui/core/colors/red';
 
 class App extends React.Component {
 
   state = {
-    memes: [],
+    weather: null,
     loading: false,
     text: '',
   }
 
-  getMemes = async (e) => {
+  getWeather = async (e) => {
     e.preventDefault()
-    this.setState({ loading: true })
-    var key = 'fuTct3EvEztztauofIqmyNiyP6YRwSjw'
-    var url = `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${key}`
+    this.setState({ loading: true, weather: null })
+    var key = 'e4acd1f8c422ae674e066d723ac0a27d'
+    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.text}&units=imperial&APPID=${key}`
     var r = await fetch(url)
     var json = await r.json()
-    this.setState({ memes: json.data, loading: false, text: '' })
+    console.log(json)
+    if (r.status === 200) {
+      this.setState({ weather: json.list, loading: false, text: '', error: null })
+    } else {
+      this.setState({ error: json.message, loading: false })
+    }
   }
 
   render() {
-    var { memes, loading, text } = this.state
+    var { weather, loading, text, error } = this.state
+
+    var data
+    if (weather) {
+      data = {
+        labels: weather.map(w => moment(w.dt * 1000).format('MMMM Do YYYY, h:mm:ss a')),
+        datasets: [{
+          label: 'Temperature',
+          borderwidth: 1,
+          data: weather.map(w => w.main.temp),
+          backgroundColor: 'rgba(132,99,255,0.2)',
+          borderColor: 'rgba(132,99,255,1)',
+          hoverBackgroundColor: 'rgba(132,99,255,0.4)',
+          hoverBorderColor: 'rgba(132,99,255,1)',
+        }]
+      }
+    }
+
     return (
       <div className="App">
-        <form className="App-header" onSubmit={this.getMemes}>
-          <Input 
-          autoFocus
-          placeholder="Search"
-          value={text}
+        <form className="App-header" onSubmit={this.getWeather}>
+          <Input
+            autoFocus
+            placeholder="Search for weather"
+            value={text}
             onChange={e => this.setState({ text: e.target.value })}
           />
-          <Button 
-          disabled={loading || !text} 
-          type="submit">
-          Enter
+          <Button
+            disabled={loading || !text}
+            type="submit">
+            Enter
           </Button>
         </form>
         <main>
-          {memes.map(meme => {
-            return <Meme key={meme.id} meme={meme} />
-          })}
-          />
-        })}
+          {data && <Bar
+            data={data}
+            width={800}
+            height={400}
+          // options={{maintainAspectRatio: false}}
+          />}
+          {error}
         </main>
       </div>
     );
   }
-}
-
-function Meme(props){
-  const {meme} = props
-  const url = meme.images.fixed_height.url
-  return (<div className="meme-wrap" onClick={()=>window.open(url, '_blank')}>
-    <img height="200" alt="meme" src={url} />
-  </div>)
 }
 
 export default App;
